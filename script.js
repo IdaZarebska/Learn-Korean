3efd74e95087fe160353c1c3c25a0ee18d0be3a1
Ida <idaendi@gmail.com>
2025-04-05 18:24:32 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+"use strict mode";
+
+// Korean numbers.
+const oneKor = "하나";
+const twoKor = "둘";
+const threeKor = "셋";
+const fourKor = "넷";
+const fiveKor = "다섯";
+const sixKor = "여섯";
+const sevenKor = "일곱";
+const eightKor = "여덟";
+const nineKor = "아홉";
+const tenKor = "열";
+
+// Chinese numbers.
+const oneChin = "일";
+const twoChin = "이";
+const threeChin = "삼";
+const fourChin = "사";
+const fiveChin = "오";
+const sixChin = "육";
+const sevenChin = "칠";
+const eightChin = "팔";
+const nineChin = "구";
+const tenChin = "십";
